409deff8a45dc222fd37f40c4e09aa0fcfa2fab2
Maverick <canxuantung@citgroup.vn>
2024-11-12 16:10:21 +0700

    fix(name-file-service): fix name file service

---
+++ .history/src/services/getDataService_20241112160953.js
@@ -0,0 +1,17 @@
+export default class GetDataService {
+    async getData() {
+        try {
+            const response = await fetch('./src/data/data.json'); 
+            if (!response.ok) {
+                throw new Error('Failed to fetch data');
+            }
+            const data = await response.json(); 
+            console.log(data)
+                                                                                                    
+            return data; 
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            throw error;
+        }
+    }
+}
